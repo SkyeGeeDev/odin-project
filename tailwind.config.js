30d4cff90a90c86204766305b8fd31c46cc03225
SkyeGeeDev <skyegeedev@gmail.com>
2023-12-27 16:24:47 -0500

    Recreated to include tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    colors: {
+      'gray': '#F8F4F9',
+      'purple': '#BEA7E5',
+      'orange': '#FFD6AF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
