9657d8a0ce4a366a70fcc3c8bb65acac4d3fccb5
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2023-12-29 11:18:43 -0500

    Completed the steak page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./public/**/**/*.html"],
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+     },
+    screens: {
+      'sm': '576px',
+      'md': '960px',
+      'lg': '1440px',
+    },
     colors: {
       'gray': '#F8F4F9',
       'purple': '#BEA7E5',
