5704c8e680cffd9916a4561b4e37d21ca058b30a
SkyeGeeDev <skyegeedev@gmail.com>
2023-12-30 18:06:05 -0500

    moved components so that index.html is in the root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/**/*.html"],
+  content: ["./**/**/**/*.html"],
   theme: {
     fontSize: {
       'xs': '.75rem',
